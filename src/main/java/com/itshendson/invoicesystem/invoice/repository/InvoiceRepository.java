2fd85966b4c11f16f84d1d561cfd1a10d13b2ae1
Hendson <hendsonszeto@live.com>
2022-10-28 22:32:22 -0700

    Modify MongoRepository to accept Id of type String to avoid bug

--- src/main/java/com/itshendson/invoicesystem/invoice/repository/InvoiceRepository.java
+++ src/main/java/com/itshendson/invoicesystem/invoice/repository/InvoiceRepository.java
@@ -3,5 +3,5 @@ package com.itshendson.invoicesystem.invoice.repository;
 import com.itshendson.invoicesystem.invoice.model.Invoice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface InvoiceRepository extends MongoRepository<Invoice, Long> {
+public interface InvoiceRepository extends MongoRepository<Invoice, String> {
 }
