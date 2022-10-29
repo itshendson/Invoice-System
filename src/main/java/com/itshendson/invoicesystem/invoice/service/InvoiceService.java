ab1a22b35c58eada59d15603850667c4f9c59b31
Hendson <hendsonszeto@live.com>
2022-10-28 22:32:56 -0700

    Implement GET request for all Invoice

--- src/main/java/com/itshendson/invoicesystem/invoice/service/InvoiceService.java
+++ src/main/java/com/itshendson/invoicesystem/invoice/service/InvoiceService.java
@@ -2,7 +2,11 @@ package com.itshendson.invoicesystem.invoice.service;
 
 import com.itshendson.invoicesystem.invoice.model.Invoice;
 
+import java.util.List;
+
 public interface InvoiceService {
 
     Invoice createInvoice(Invoice invoice);
+
+    List<Invoice> getAllInvoice();
 }
--- src/main/java/com/itshendson/invoicesystem/invoice/service/InvoiceServiceImpl.java
+++ src/main/java/com/itshendson/invoicesystem/invoice/service/InvoiceServiceImpl.java
@@ -5,6 +5,8 @@ import com.itshendson.invoicesystem.invoice.repository.InvoiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class InvoiceServiceImpl implements InvoiceService{
 
@@ -12,7 +14,12 @@ public class InvoiceServiceImpl implements InvoiceService{
     private InvoiceRepository invoiceRepository;
 
     @Override
+    public List<Invoice> getAllInvoice() {
+        return invoiceRepository.findAll();
+    }
+
+    @Override
     public Invoice createInvoice(Invoice invoice) {
-        return invoiceRepository.insert(invoice);
+        return invoiceRepository.save(invoice);
     }
 }
