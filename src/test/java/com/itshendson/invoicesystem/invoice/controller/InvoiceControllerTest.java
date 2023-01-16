9fc6404b712b8635b9f4a5f798ecd00cc446c4f4
Hendson <hendsonszeto@live.com>
2023-01-15 21:15:01 -0800

    Rewrite InvoiceController web layer tests to be cleaner

--- src/test/java/com/itshendson/invoicesystem/invoice/controller/InvoiceControllerTest.java
+++ src/test/java/com/itshendson/invoicesystem/invoice/controller/InvoiceControllerTest.java
@@ -1,23 +1,21 @@
 package com.itshendson.invoicesystem.invoice.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itshendson.invoicesystem.invoice.exception.InvoiceNotFoundException;
 import com.itshendson.invoicesystem.invoice.model.Invoice;
 import com.itshendson.invoicesystem.invoice.service.InvoiceServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(InvoiceController.class)
@@ -26,80 +24,70 @@ class InvoiceControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @MockBean
     private InvoiceServiceImpl invoiceServiceImplMock;
 
-    private Invoice dummyInvoice;
+    private Invoice invoice1;
+    private Invoice invoice2;
+    private ObjectMapper mapper = new ObjectMapper();
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
+        invoice1 = new Invoice(null, "company1", new BigDecimal(0), new ArrayList<>());
+        invoice2 = new Invoice(null,  null, new BigDecimal(0), new ArrayList<>());
     }
 
     @Test
-    void getAllInvoice() throws Exception{
+    void testGetAllInvoice() throws Exception{
         mockMvc.perform(get("/api/v1/invoice")
                 .contentType("application/json"))
                 .andExpect(status().isOk());
     }
 
     @Test
-    void whenCreateValidInvoice_returnStatus201() throws Exception {
-        dummyInvoice = new Invoice(null, "ABC Inc.", new BigDecimal(0), new ArrayList<>());
-        String body = objectMapper.writeValueAsString(dummyInvoice);
-
+    void testCreateInvoiceSuccess() throws Exception {
         mockMvc.perform(post("/api/v1/invoice")
                         .contentType("application/json")
-                        .content(body))
+                        .content(mapper.writeValueAsString(invoice1)))
                 .andExpect(status().isCreated());
     }
 
     @Test
-    void givenTwoLetterName_whenCreateInvoice_returnStatus400() throws Exception {
-        dummyInvoice = new Invoice(null, "ab", new BigDecimal(0), new ArrayList<>());
-        String body = objectMapper.writeValueAsString(dummyInvoice);
-
+    void testCreateInvoiceBadRequest() throws Exception {
         mockMvc.perform(post("/api/v1/invoice")
                         .contentType("application/json")
-                        .content(body))
+                        .content(mapper.writeValueAsString(invoice2)))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
-    void givenEmptyName_whenCreateInvoice_returnStatus400() throws Exception {
-        dummyInvoice = new Invoice(null, "", new BigDecimal(0), new ArrayList<>());
-        String body = objectMapper.writeValueAsString(dummyInvoice);
-
-        mockMvc.perform(post("/api/v1/invoice")
-                .contentType("application/json")
-                .content(body))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    void whenFindInvoiceByExistingId_returnStatus200() throws Exception {
-        mockMvc.perform(get("/api/v1/invoice/{id}", "1S")
+    void testFindInvoiceByIdSuccess() throws Exception {
+        mockMvc.perform(get("/api/v1/invoice/{id}", "111111")
                 .contentType("application/json"))
                 .andExpect(status().isOk());
     }
 
     @Test
-    void whenDeleteExistingInvoice_returnStatus200() throws Exception {
-        mockMvc.perform(delete("/api/v1/invoice/{id}", "1S")
+    void testFindInvoiceByIdNotFound() throws Exception {
+        when(invoiceServiceImplMock.findInvoiceById("111111")).thenThrow(new InvoiceNotFoundException());
+
+        mockMvc.perform(get("/api/v1/invoice/{id}", "111111")
+                        .contentType("application/json"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testDeleteInvoiceSuccess() throws Exception {
+        mockMvc.perform(delete("/api/v1/invoice/{id}", "111111")
                 .contentType("application/json"))
                 .andExpect(status().isOk());
     }
 
     @Test
-    void whenUpdateExistingInvoice_returnStatus200() throws Exception {
-        dummyInvoice = new Invoice(null, "update to this name", new BigDecimal(0), new ArrayList<>());
-        String body = objectMapper.writeValueAsString(dummyInvoice);
-
+    void testUpdateInvoiceSuccess() throws Exception {
         mockMvc.perform(put("/api/v1/invoice/{id}", "1S")
                 .contentType("application/json")
-                .content(body))
+                .content(mapper.writeValueAsString(invoice1)))
                 .andExpect(status().isOk());
     }
 }
